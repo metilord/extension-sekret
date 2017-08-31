6fe49ba530a2d8c733490b102c9e99309f1d9a19
Liam Kellogg <lkellogg22@j8-c02px0eygl28.punahou.edu>
2017-08-31 13:35:17 -1000

    Wow Carson you legend

--- newtab.js
+++ newtab.js
@@ -1,1 +1,19 @@
-console.log("hello")
+var mouseX =
+
+console.log(mouseX);
+
+document.getElementById('QT1').onclick = function() {
+	window.location = "https://classroom.google.com/h";
+}
+
+document.getElementById('QT2').onclick = function() {
+	window.location = "https://quizlet.com";
+}
+
+document.getElementById('QT3').onclick = function() {
+	window.location = "https://drive.google.com/drive/my-drive";
+}
+
+document.getElementById('QT4').onclick = function() {
+	window.location = "https://github.com";
+}
