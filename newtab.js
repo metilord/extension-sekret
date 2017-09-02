b242a146ca03a86606ad87157118ba72801c8e6d
Junior DevLeague <juniordevleague@JDLA053.local>
2017-09-01 17:52:50 -1000

    Trying to get a settings icon

--- newtab.js
+++ newtab.js
@@ -1,18 +1,30 @@
-/*if (typeof(Storage) !== undefined) {
-	if (localStorage.urlList) {
-		
+var rectAttr = ['25', '8', '9', '13', '2', '2'];
+
+var custom_menu = document.getElementById('rightClick');
+function CustomMenu(T) {
+	if (T === "T") {
+		custom_menu.style.display = "block";
+		custom_menu.focus();
 	} else {
-		localStorage.urlList = ["", "", "", ""];
+		custom_menu.style.display = "none";
 	}
-}*/
-function CustomMenu() {
-	
 }
 
-
-window.oncontextmenu = function() {
-	CustomMenu();
+window.oncontextmenu = function(e) {
+	custom_menu.style.left = e.clientX + 'px';
+	custom_menu.style.top = e.clientY + 'px';
+	CustomMenu('T');
 	return false;
+}
+
+window.onclick = function() {
+	CustomMenu('F');
+}
+
+window.onkeydown = function(e) {
+	if (e.keyCode === 27) {
+		CustomMenu('F');
+	}
 }
 
 document.getElementById('QT1').onclick = function() {
@@ -30,3 +42,18 @@ document.getElementById('QT3').onclick = function() {
 document.getElementById('QT4').onclick = function() {
 	window.location = "https://github.com";
 }
+
+window.onload = function() {
+	var x = document.getElementsByTagName('svg')[0];
+	var y = document.createElement('rect');
+	y.setAttribute('x', '25');
+	y.setAttribute('y', '8');
+	y.setAttribute('width', '9');
+	y.setAttribute('height', '13');
+	y.setAttribute('rx', '2');
+	y.setAttribute('ry', '2');
+	y.setAttribute('fill', 'rgb(60, 60, 60)');
+	y.setAttribute('transform', 'rotate(45, 30, 30)');
+	x.appendChild(y);
+}
+
