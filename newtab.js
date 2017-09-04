95af12da2ecb652445ae5d8e239f5e04e76a6dd4
Liam Kellogg <lkellogg22@j8-c02px0eygl28.gateway.ht.net>
2017-09-03 15:32:42 -1000

    Working on quickTabbing

--- newtab.js
+++ newtab.js
@@ -1,7 +1,8 @@
 var select = false;
 var custom_menu = document.getElementById('rightClick');
-var CLASSROOMDOM = document.getElementsByTagName('svg')[0];
 var createElem;
+//var quickTab = document.getElementsByClassName('quickTab');
+var linkList = ['https://classroom.google.com/h', 'https://quizlet.com', 'https://drive.google.com/drive/my-drive', 'https://github.com'];
 
 function CustomMenu(T) {
 	if (T === "T") {
@@ -16,7 +17,7 @@ function Draw(Type) {
 	if (Type == "settings_icon") {
 		var rectVal = ['26', '12', '7', '8', '2', '2', 'rgb(60, 60, 60)'];
 		var rectAttr = ['x', 'y', 'width', 'height', 'rx', 'yx', 'fill'];
-		var SETTINGDOM = document.getElementsByTagName('svg')[1];
+		var SETTINGDOM = document.getElementsByTagName('svg')[0];
 
 		for (var z = 0; z < 9; z ++) {
 			createElem = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
@@ -29,32 +30,45 @@ function Draw(Type) {
 			SETTINGDOM.appendChild(createElem);
 		}
 	}
+}
 
-	if (Type == "classroom_icon") {
-		var classroomRectAttr = [];
-		var classroomRectVal = [];
-		createElem = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-		for (var i = 0; i < classroomRectVal.length; i ++) {
-			createElem.setAttribute(classroomRectAttr[i], classroomRectVal[i])
-		}
+function quickTabHndler() {
+	for (var i = 0; i < linkList.length; i ++) {
+		var theDom = document.getElementById('quickTabDiv');
+
+		createElem = document.createElement("anchor");
+		createElem.setAttribute("href", "https://google.com");
+		createElem.setAttribute("id", "anchor" + i);
+		createElem.setAttribute("display", "block");
+		theDom.appendChild(createElem);
+
+
+		theDom = document.getElementById("anchor" + i);
+		
+		createElem = document.createElement("div");
+		createElem.setAttribute("class", "quickTab");
+		createElem.setAttribute("tabindex", i + 2);
+		createElem.setAttribute("display", "block");
+		//createElem.setAttribute("id", "QT" + (i + 1));
+		theDom.appendChild(createElem);
 	}
 }
 
-document.getElementById('QT1').onclick = function() {
-	window.location = "https://classroom.google.com/h";
+/*document.getElementById('QT1').onclick = function() {
+	window.location = linkList[0];
 }
 
 document.getElementById('QT2').onclick = function() {
-	window.location = "https://quizlet.com";
+	window.location = linkList[1];
 }
 
 document.getElementById('QT3').onclick = function() {
-	window.location = "https://drive.google.com/drive/my-drive";
+	window.location = linkList[2];
 }
 
 document.getElementById('QT4').onclick = function() {
-	window.location = "https://github.com";
-}
+	window.location = linkList[3];
+}*/
 
 window.oncontextmenu = function(e) {
 	custom_menu.style.left = e.clientX + 'px';
@@ -70,9 +84,7 @@ window.onclick = function() {
 window.onkeydown = function(e) {
 	if (e.keyCode === 27) {
 		CustomMenu('F');
-	}
-
-	if (select === false) {
+	} else if (select === false) {
 		document.getElementById('searchBox').focus();
 		select = true;
 	}
@@ -80,6 +92,6 @@ window.onkeydown = function(e) {
 
 window.onload = function() {
 	Draw('settings_icon');
-	
+	quickTabHndler();
 }
 
